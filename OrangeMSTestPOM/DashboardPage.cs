65dbe0e86ee8e70bf528a087265aece3d26bed38
HasiniIshara <hasiniisharao4@gmail.com>
2024-02-10 18:26:50 +0200

    Improve DashboardPage readability.

--- OrangeMSTestPOM/DashboardPage.cs
+++ OrangeMSTestPOM/DashboardPage.cs
@@ -7,18 +7,23 @@ using System.Threading.Tasks;
 
 namespace OrangeMSTestPOM
 {
+    //Create Class
     public class DashboardPage
     {
+        //Create web driver object
         private readonly IWebDriver driver;
 
+        //Call DashboardPage Constructor
         public DashboardPage(IWebDriver driver)
         {
             this.driver = driver;
         }
 
+        //Create DashboardPage web elements
         public IWebElement AdminNaviBtn => driver.FindElement(By.XPath("//*[@id=\"menu_item_101\"]/span"));
 
 
+        //Navigate to the Admin Module
         public AdminPage NavigateAdminModule() {
             AdminNaviBtn.Click();
             return new AdminPage(driver);
