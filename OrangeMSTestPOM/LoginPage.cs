582fb502f864a37f0ee1a12ea6cd65cbd30a7b43
HasiniIshara <hasiniisharao4@gmail.com>
2024-02-10 18:25:17 +0200

    Improve readability of LoginPage.

--- OrangeMSTestPOM/LoginPage.cs
+++ OrangeMSTestPOM/LoginPage.cs
@@ -10,17 +10,21 @@ namespace OrangeMSTestPOM
     //class creating
     public class LoginPage
     {
+        //Create web driver object
         private readonly IWebDriver driver;
 
+        //Call LoginPage Constructor
         public LoginPage(IWebDriver driver)
         {
             this.driver = driver;
         }
 
+        //Create LoginPage web elements
         public IWebElement UserName => driver.FindElement(By.Id("txtUsername"));
         public IWebElement UserPassword => driver.FindElement(By.Id("txtPassword"));
         public IWebElement LoginButton => driver.FindElement(By.XPath("//*[@id=\"frmLogin\"]/div[4]/button"));
 
+        //Login function
         public DashboardPage Login(string userKey)
         {
             string UName = SystemUtil.GetUsername(userKey);
